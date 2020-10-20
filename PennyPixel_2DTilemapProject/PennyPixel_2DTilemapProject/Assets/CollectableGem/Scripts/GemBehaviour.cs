46f4d862d0d0126f8d9d2646dc8bc5cd9c3b99a0
7riLo <lwyant@mail.bradley.edu>
2020-10-20 17:49:05 -0500

    2d Prototype

---
+++ PennyPixel_2DTilemapProject/PennyPixel_2DTilemapProject/Assets/CollectableGem/Scripts/GemBehaviour.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Levi Wyant
+ * 2D Prototype
+ * Most of this code was provided, however it was modified for the assignment
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemBehaviour : MonoBehaviour
+{
+	[Header("References")]
+	public GameObject gemVisuals;
+	public GameObject collectedParticleSystem;
+	public CircleCollider2D gemCollider2D;
+	private DisplayScore displayScoreScript;
+
+	private float durationOfCollectedParticleSystem;
+
+
+	void Start()
+	{
+		durationOfCollectedParticleSystem = collectedParticleSystem.GetComponent<ParticleSystem>().main.duration;
+		//added in code to set up displayScoreScript update
+		displayScoreScript = GameObject.FindGameObjectWithTag("DisplayScoreText").GetComponent<DisplayScore>();
+	}
+
+	void OnTriggerEnter2D(Collider2D theCollider)
+	{
+		if (theCollider.CompareTag ("Player")) 
+		{
+			GemCollected ();
+			//add to +1 score
+			displayScoreScript.score++;
+		}
+	}
+
+	void GemCollected()
+	{
+		gemCollider2D.enabled = false;
+		gemVisuals.SetActive (false);
+		collectedParticleSystem.SetActive (true);
+		Invoke ("DeactivateGemGameObject", durationOfCollectedParticleSystem);
+		
+
+	}
+
+	void DeactivateGemGameObject()
+	{
+		gameObject.SetActive (false);
+	}
+}
---
+++ PennyPixel_2DTilemapProject/PennyPixel_2DTilemapProject/Assets/GothicKnight/Scripts/DisplayScore.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Levi Wyant
+ * 2D Prototype
+ * Display the score via text box script
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngineInternal;
+
+public class DisplayScore : MonoBehaviour
+{
+    public Text textbox;
+
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set text component on start
+        textbox = GetComponent<Text>();
+        textbox.text = "Score: 0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textbox.text = "Score: " + score;
+        if(score >= 13)
+        {
+            textbox = GetComponent<Text>();
+            textbox.text = "You Win!";
+        }
+    }
+}
