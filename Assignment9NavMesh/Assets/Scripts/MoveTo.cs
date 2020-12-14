6f49fdf4fb59c300cee7ef5aca49d30d2a072fab
7riLo <lwyant@mail.bradley.edu>
2020-12-14 00:31:15 -0600

    update
    1

---
+++ Assignment9NavMesh/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,28 @@
+﻿/*Levi Wyant   
+ * CIS 497
+ * MoveTo
+ * Moves player object to
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+    public Transform goal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
